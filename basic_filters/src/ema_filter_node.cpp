8e787f20b0d21e288133a8b3648f3312159ea1e2
arthurgomes4 <arthurgomes2727@gmail.com>
2022-12-13 21:04:08 +0530

    multi-topic filtering achieved

--- basic_filters/src/ema_filter_node.cpp
+++ basic_filters/src/ema_filter_node.cpp
@@ -47,6 +47,7 @@ class ROS_EMAfilter
         m_filter.setSmoothingFactor(smoothingFactor);
         m_publisher = nh.advertise<std_msgs::Float64>(outputTopic, 10);
         m_subscriber = nh.subscribe(inputTopic, 10, &ROS_EMAfilter::callback, this);
+        ROS_INFO("raw:/%s | filtered:/%s | smoothing:%f",inputTopic, outputTopic, smoothingFactor);
     }
 
     void callback(std_msgs::Float64 message)
@@ -54,7 +55,7 @@ class ROS_EMAfilter
         float rawValue = message.data;
         message.data = m_filter.filterValue(rawValue);
         m_publisher.publish(message);
-        ROS_INFO("received: %f published: %f", rawValue, message.data);
+        // ROS_INFO("received: %f published: %f", rawValue, message.data);
     }
 };
 
@@ -62,8 +63,44 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "ema_filter_node");
     ros::NodeHandle nh;
-    ROS_EMAfilter joint1(nh, "joint1_command", "joint1_filtered", 0.3);
-    ROS_INFO("ready for data");
+
+    if(!ros::param::has("~smoothingFactors")){
+        ROS_ERROR("private param <smoothingFactors> not found\nThis is a list of floats\neach topic will have its own smoothingfactor");
+        ros::shutdown();
+        return 0;
+    }
+    if(!ros::param::has("~inputTopics")){
+        ROS_ERROR("private param <inputTopics> not found\nThis is a list of strings");
+        ros::shutdown();
+        return 0;
+    }
+    if(!ros::param::has("~outputTopics")){
+        ROS_ERROR("private param <outputTopics> not found\n this is a list of strings");
+        ros::shutdown();
+        return 0;
+    }
+
+    std::vector<std::string> inputTopics;
+    std::vector<std::string> outputTopics;
+    std::vector<float> smoothingFactors;
+
+    ros::param::get("~smoothingFactors", smoothingFactors);
+    ros::param::get("~inputTopics", inputTopics);
+    ros::param::get("~outputTopics", outputTopics);
+
+    std::vector<ROS_EMAfilter*> filters;
+
+    for(int i=0; i<inputTopics.size(); i++)
+    {
+        filters.push_back(new ROS_EMAfilter(nh, inputTopics[i].c_str(), outputTopics[i].c_str(), smoothingFactors[i]));
+    }
+
+    ROS_INFO("node setup complete.");
     ros::spin();
+
+    for(ROS_EMAfilter* ptr : filters)
+    {
+        delete ptr;
+    }
     return 0;
 }
